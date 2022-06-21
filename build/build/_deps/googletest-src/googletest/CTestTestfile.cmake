3ac39ed00628a646a3ac4d05c2d9cc05922b2168
John Bryan Valle <jbvalle.jv@gmail.com>
2022-06-21 20:06:59 +0200

    upd yml

--- build/build/_deps/googletest-src/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jv/Coding/gruppe_v/build/_deps/googletest-src/googletest
-# Build directory: /home/jv/Coding/gruppe_v/build/build/_deps/googletest-src/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
